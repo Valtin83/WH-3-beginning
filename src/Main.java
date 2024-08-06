b2b7fb05992f3e96be32867446995a46c41391b0
Valentin <zhe-valentin@mail.ru>
2024-08-07 00:50:41 +0700

    WH-3 beginning 1-6

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
             System.out.println("На улице " + temp +
                     " градусов, нужно надеть шапку.");
 
-        }else {
+        } else {
             System.out.println("«На улице " + temp +
                     " градусов, можно идти без шапки.");
         }
@@ -29,13 +29,59 @@ public class Main {
         if (speed > 60) {
             System.out.println("Если скорость " + speed +
                     " км/ч, то придется заплатить штраф.");
-        }else {
+        } else {
             System.out.println("Если скорость " + speed +
                     " км/ч, то можно ездить спокойно.");
         }
 
         System.out.println(" Задаа №4");
+        int ageOfStay = 15;
+        if (ageOfStay >= 2 && ageOfStay <= 6) {
+            System.out.println("Если возраст человека равен " + ageOfStay +
+                    " , то ему нужно ходить в детский сад.");
+        }
+        if (ageOfStay >= 7 && ageOfStay <= 17) {
+            System.out.println("Если возраст человека равен " + ageOfStay +
+                    ", то ему нужно ходить в школу.");
+        }
+        if (ageOfStay >= 18 && ageOfStay <= 24) {
+            System.out.println("Если возраст человека равен " + ageOfStay +
+                    ",его место в университете.");
+        }
+        if (ageOfStay > 24) {
+            System.out.println("Время ходить на работу.");
+        }
 
+        System.out.println("Задача №5");
+        int ageChild = 10;
+        int escort = 1;
+        if (ageChild < 5) {
+            System.out.println("Если возраст ребенка равен " + ageChild +
+                    ", то ему не рвзрешено кататься на аттракционе.");
+        }
+        if (ageChild >= 5 && ageChild <= 14 && escort >= 1) {
+            System.out.println("Если возраст ребенка равен " + ageChild +
+                    ", то можно кататься на аттракционе в сопровождении.");
+        } else {
+            System.out.println("Без сопровождения катататься нельзя.");
+        }
+        if (ageChild > 14) {
+            System.out.println("Если возраст ребенка равен " + ageChild +
+                    ", то можо кататься без сопровождения взрослого");
+        }
 
+        System.out.println("Задача №6");
+        int passengers = 104;
+        int carCapacity = 102;
+        int seating = 60;
+        if (passengers <= 60) {
+            System.out.println("В вагоне есть " + (seating - passengers) +
+                    "  сидячих мест и " + (carCapacity -seating) + " стоячих.");
+        } else {
+            System.out.println("Сидячих мест 0. Стоячих мест  " + (carCapacity - passengers));
+        }
+        if (passengers  > 102) {
+            System.out.println("Вагон переполнен.");
+        }
     }
 }
